559ca58ac77d56d072bcad82d7a3b0a6b0cd169e
Upwards-rwr <1512501934@qq.com>
2023-04-01 15:00:45 +0800

    509.斐波那契数

---
+++ 509.斐波那契数/509.斐波那契数.ts
@@ -0,0 +1,14 @@
+// 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+
+// F(0) = 0，F(1) = 1
+// F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+function fib(n:number):number{
+    const dp = []
+    dp[0] = 0;
+    dp[1] = 1;
+    if(n<2) return n
+    for(let i = 2; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    return dp[n]
+}
